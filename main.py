831b18f956b8407de7c2c1885d9adecba50da440
Yixin Lu <lu.ericy@outlook.com>
2026-02-18 15:37:18 +0900

    Initial upload

---
+++ main.py
@@ -0,0 +1,16 @@
+def encrypt(message, key):
+    #encrypt the message
+    return 
+
+def decrypt(encrypted_message, key):
+    #decrypt the message
+    return
+
+def UI():
+    #make ui
+    return
+
+def main():
+    print("Finished")
+    
+main()
